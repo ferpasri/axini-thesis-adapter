121f0dac1ace2e431af173d18319d5bf54096949
asirre <goss.janeczko@gmail.com>
2023-05-05 16:48:41 +0200

    add generic update

--- plugin_adapter_components/client_side/sut.py
+++ plugin_adapter_components/client_side/sut.py
@@ -37,49 +37,27 @@ class SeleniumSut:
     def click(self, css_selector):
         self.browser.find_by_css(css_selector).first.click()
         time.sleep(2)
-
-
-    def expect_page_to_have(self, css_selector, title=None):
-        if title:
-            element = self.browser.is_element_present_by_css(css_selector, text=title)
-        else:
-            element = self.browser.is_element_present_by_css(css_selector)
-
-        response = ["expect_page_to_have", { "found": "string" }, {"found": element}]
-        return self.handle_response(response)
-
-
-    def expect_page_not_to_have(self, css_selector, title=None):
-        if title:
-            element = self.browser.is_element_present_by_css(css_selector, text=title)
-        else:
-            element = self.browser.is_element_present_by_css(css_selector)
-        response = ["expect_page_not_to_have", { "not_found": "string"}, {"not_found": element}]
-        return self.handle_response(response)
+        self.generate_page_update_response()
 
 
     def visit(self, url):
         self.browser.visit(url)
         time.sleep(2)
-
-
-    def get_url(self):
-        response = ["get_url", {"_url" : "string"}, {"_url": self.browser.url}]
-        return self.handle_response(response)
-
-
-    def get_value(self, css_selector):
-        value = self.browser.find_by_css(css_selector).first.value
-        response = ["get_value", { "css_selector": "string", "value": "string" }, {"css_selector": css_selector, "value": value}]
-        return self.handle_response(response)
+        self.generate_page_update_response()
 
 
     def fill_in(self, css_selector, value):
         self.browser.find_by_css(css_selector).fill(value)
         time.sleep(2)
+        self.generate_page_update_response()
 
 
     def start(self):
         self.browser = Browser('chrome')
         self.browser.visit("https://academybugs.com/find-bugs/")
         WebDriverWait(self.browser.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body.loaded')))
+
+
+    def generate_page_update_response(self):
+        response = ["page_update", {"_html": "string", "_url": "string"}, {"_html": self.browser.html, "_url": self.browser.url}]
+        self.handle_response(response)
